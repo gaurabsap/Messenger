bee5f860c4fe2437f9aa32153f2dce20d5f63e19
gb_gaurab19 <86666317+gaurabsap@users.noreply.github.com>
2023-02-03 22:01:16 +0545

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAA1y5cLWyC_2I2QOFMsXPXXoIHhwgu8ds",
-  authDomain: "pr-firebase-8d1d0.firebaseapp.com",
-  projectId: "pr-firebase-8d1d0",
-  storageBucket: "pr-firebase-8d1d0.appspot.com",
-  messagingSenderId: "145873698495",
-  appId: "1:145873698495:web:876bb99cc001525035d00a"
+  apiKey: "a",
+  authDomain: "b",
+  projectId: "c",
+  storageBucket: "x",
+  messagingSenderId: "n",
+  appId: "xx"
 };
 
 const app = initializeApp(firebaseConfig);
